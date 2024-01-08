9b8d3a303a1d1b145f4dff3e531987b3bcc31a7f
xXcodeXayushXx <codexayush@gmail.com>
2024-01-08 10:40:58 +0530

    Made the following changes: - created a main class - sorted array elements in main function

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+
+public class Main
+{   
+    public static void main(String args[])
+    {
+        List<Integer> l=Arrays.asList(20,19,10,5,4);
+        Collections.sort(l);
+        Iterator<Integer> itr= l.iterator();
+
+        while(itr.hasNext())
+        {
+            System.out.println(itr.next());
+        }
+        
+    }
+}
